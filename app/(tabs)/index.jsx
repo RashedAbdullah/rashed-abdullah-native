405bb4ba00fbc7778e656054f74b9fdfe4ffdc9f
RashedAbdullah <maarashed40@gmail.com>
2024-12-04 18:22:11 +0600

    makeing home screen more effective

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,28 @@
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import React from "react";
+
+const HomeScreen = () => {
+  return (
+    <ScrollView className="min-h-screen bg-[#C5DEFE] dark:bg-[#0f172a] px-6 py-10">
+      {/* Profile Picture */}
+      <View className="items-center my-10">
+        <Image
+          source={require("@/assets/images/logo.png")}
+          className="w-32 h-32 rounded-full border-2 border-[#0f172a] dark:border-white"
+        />
+      </View>
+
+      {/* Header Section */}
+      <View className="mb-6">
+        <Text className="text-3xl font-bold text-[#0f172a] dark:text-white text-center">
+          Rashed Abdullah
+        </Text>
+        <Text className="text-gray-700 dark:text-gray-300 mt-1 text-center">
+          Front-End Developer
+        </Text>
+      </View>
+    </ScrollView>
+  );
+};
+
+export default HomeScreen;
